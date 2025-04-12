318af6ea5381dd00e16258aaa47343ac3b572ab4
USER <USER@ETAIPC>
2025-04-12 13:13:46 +0300

    DONE! [+] Created cleanup [+] Added  status

--- DLL Injection 1 - Create Remote Thread/main.c
+++ DLL Injection 1 - Create Remote Thread/main.c
@@ -6,10 +6,12 @@
 
 int main(int argc, char* argv[])
 {
+    StatusCode status = STATUS_ERROR;
     if (MIN_NUM_OF_ARGUMENTS > argc)
     {
         printf("<Please Enter 2 arguments: DLL Path to inject and PID>");
-        return INVALID_NUMBER_OF_ARGUMENTS;
+        status = INVALID_NUMBER_OF_ARGUMENTS;
+        goto cleanup;
     }
     /* Initialization */
     DWORD target_process_pid = INITIAL_PID;
@@ -17,11 +19,10 @@ int main(int argc, char* argv[])
     LPVOID remote_process_memmory_space = INVALID_BASE_MEMMORY_ADDRESS;
     SIZE_T size_to_allocate = INVALID_MEMMORY_SIZE_TO_RESERVE;
     BOOL memmory_write_status = MEMMORY_WRITING_FAILED;
+    SIZE_T bytes_written = 0;
     HMODULE kernel32dll_handle = INVALID_MODULE_HANDLE;
     FARPROC load_library_address = INVALID_FUNCTION_ADDRESS;
     HANDLE remote_thread_handle = INVALID_THREAD_HANDLE;
-    DWORD remote_thread_id = 0;
-    DWORD remote_thread_status_code = 0;
 
     /* Converting the PID to int */
     target_process_pid = atoi(argv[PID]);
@@ -29,15 +30,17 @@ int main(int argc, char* argv[])
     if (INITIAL_PID == target_process_pid)
     {
         printf("<INVALID PID>");
-        return INVALID_PID;
+        status = INVALID_PID;
+        goto cleanup;
     }
 
     /* Getting the Size of the DLL Path to Inject */
-    size_to_allocate = strnlen_s(argv[DLL_TO_RUN], MAXIMUM_PATH_SIZE) + 1;
+    size_to_allocate = strnlen_s(argv[DLL_TO_RUN], MAXIMUM_PATH_SIZE) + 1; // +1 for Null character
     if (MAXIMUM_PATH_SIZE == size_to_allocate || INVALID_MEMMORY_SIZE_TO_RESERVE == size_to_allocate)
     {
         printf("<INVALID DLL PATH SIZE>");
-        return INVALID_DLL_PATH_SIZE;
+        status = INVALID_DLL_PATH_SIZE;
+        goto cleanup;
     }
 
     /* Get Process Handle */
@@ -49,7 +52,8 @@ int main(int argc, char* argv[])
     if (INVALID_PROCESS_HANDLE == target_process_handle)
     {
         printf("<Could'nt open process handle>");
-        return FAILED_TO_GET_PROCESS_HANDLE;
+        status = FAILED_TO_GET_PROCESS_HANDLE;
+        goto cleanup;
     }
 
     /* Aloocating Memmory in the Remote Process */
@@ -63,7 +67,8 @@ int main(int argc, char* argv[])
     if (INVALID_BASE_MEMMORY_ADDRESS == remote_process_memmory_space)
     {
         printf("<ALLOCATION FAILED>");
-        return FAILED_TO_ALLOCATE_REMOTE_MEMMORY;
+        status = FAILED_TO_ALLOCATE_REMOTE_MEMMORY;
+        goto cleanup;
     }
 
     /* Writing Memmory in the Remote Process */
@@ -72,21 +77,23 @@ int main(int argc, char* argv[])
         remote_process_memmory_space,
         argv[DLL_TO_RUN],
         size_to_allocate,
-        NULL);
-    if (MEMMORY_WRITING_FAILED == memmory_write_status)
+        &bytes_written);
+    if (MEMMORY_WRITING_FAILED == memmory_write_status || bytes_written != size_to_allocate)
     {
         printf("<MEMMORY WRITING FAILED>");
-        return FAILED_TO_WRITE_MEMMORY;
+        status = FAILED_TO_WRITE_MEMMORY;
+        goto cleanup;
     }
 
     /* Get the DLL Handle */
     kernel32dll_handle = GetModuleHandle(
-        TEXT("kernel32.dll"));
+        "kernel32.dll");
 
     if (INVALID_MODULE_HANDLE == kernel32dll_handle)
     {
         printf("<FAILED TO GET DLL HANDLE>");
-        return FAILED_TO_GET_MODULE_HANDLE;
+        status = FAILED_TO_GET_MODULE_HANDLE;
+        goto cleanup;
     }
 
     /* Get the Base Address of LoadLibraryA */
@@ -96,7 +103,8 @@ int main(int argc, char* argv[])
     if (INVALID_FUNCTION_ADDRESS == load_library_address)
     {
         printf("<FAILED TO GET FUNCTION ADDRESS>");
-        return FAILED_TO_GET_FUNCTION_ADDRESS;
+        status = FAILED_TO_GET_FUNCTION_ADDRESS;
+        goto cleanup;
     }
 
     /* Creating a Remote Thread */
@@ -107,18 +115,19 @@ int main(int argc, char* argv[])
         load_library_address,
         (LPCSTR) remote_process_memmory_space,
         0,
-        &remote_thread_id);
+        NULL);
 
     if (INVALID_THREAD_HANDLE == remote_thread_handle)
     {
         printf("<FAILED TO CREATE REMOTE THREAD>");
-        return FAILED_TO_CREATE_REMOTE_THREAD;
+        status = FAILED_TO_CREATE_REMOTE_THREAD;
+        goto cleanup;
     }
-    printf("Remote Thread ID: %lu\n", remote_thread_id);
-    GetExitCodeThread(remote_thread_handle, &remote_thread_status_code);
-    printf("Exit Code: %lu\n", remote_thread_status_code);
+    status = STATUS_SUCCESS;
 
-    return STATUS_SUCCESS;
+cleanup:
+
+    return status;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
